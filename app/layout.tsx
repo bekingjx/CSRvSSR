e395e448d4d269580f99a4c815ec63074a2c7434
bekingjx <lorenzo.cavicchioli27798@gmail.com>
2023-05-27 10:33:32 +0200

    CSRvsSSR

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,7 @@ export const metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+  children,}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className='first-app'>
